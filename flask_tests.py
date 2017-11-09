99ab6d069fba5eca679d50a961a797e4c6e0c1f8
risleychris <risley.c@husky.neu.edu>
2017-11-09 14:20:06 -0500

    Added 'About'

--- flask_tests.py
+++ flask_tests.py
@@ -3,9 +3,7 @@ from flask_testing import TestCase
 import unittest
 
 
-
 class TopSubTest(TestCase):
-
     def create_app(self):
         app = Flask(__name__)
         app.config['Testing'] = True
@@ -15,7 +13,6 @@ class TopSubTest(TestCase):
         client = self.create_app().test_client(True)
         response = self.client.get("/topsubs/api/v1.0/news/top/20")
         print(response)
-
 
 
 if __name__ == '__main__':
